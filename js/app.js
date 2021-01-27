1b4582477924b8621c435b5000159a34d3984d33
Umedzhon Nazriev <zabirovi1221@gmail.com>
2021-01-27 13:10:50 +0800

    add all file to git

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const resMenu = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    resMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
